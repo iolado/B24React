abf3aec2dc56be1dc2efbfa516d548113ea5cba7
iolado <oladokui@oregonstate.edu>
2026-02-10 20:25:55 -0800

    React page

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+  useEffect(() => {
+    if (show && show.episodes.length > 0) {
+      setSelectedEpisode(show.episodes[0]);
+    } else {
+      setSelectedEpisode(null);
+    }
+  }, [show]);
+
+  
+  if (!show) {
+
+  return (
+      <div className="show-details">
+        <p>Please select a show to view its episodes.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="show-details">
+
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
